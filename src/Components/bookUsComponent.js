04a901808b44950051bed726a652fe7a2830a1f5
Stensis <irenenjuguna98@gmail.com>
2022-10-12 10:57:53 +0300

    added contact form component

--- src/Components/bookUsComponent.js
+++ src/Components/bookUsComponent.js
@@ -1,30 +1,30 @@
-// // imports
-// import React, { useState } from "react";
-// import BookingForm from "./bookingFormComponent";
+// imports
+import React, { useState } from "react";
+import BookingForm from "./bookingFormComponent";
 
-// // booking button
-// function BookUs() {
-//   const [visibleForm, setVisibleForm] = useState(false);
-//   const showForm = () => {
-//     setVisibleForm(true);
-//   };
-//   return (
-//     <div className="d-flex justify-content-center mt-3">
-//       {visibleForm === true ? (
-//         <BookingForm />
-//       ) : (
-//         <div className="d-grid gap-2">
-//           <button
-//             className="btn btn-outline-success text-nowrap"
-//             type="button"
-//             onClick={showForm}
-//           >
-//             Book Us
-//           </button>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
+// booking button
+function BookUs() {
+  const [visibleForm, setVisibleForm] = useState(false);
+  const showForm = () => {
+    setVisibleForm(true);
+  };
+  return (
+    <div className="d-flex justify-content-center mt-3">
+      {visibleForm === true ? (
+        <BookingForm />
+      ) : (
+        <div className="d-grid gap-2">
+          <button
+            className="btn btn-outline-success text-nowrap"
+            type="button"
+            onClick={showForm}
+          >
+            Book Us
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
 
-// export default BookUs;
+export default BookUs;
