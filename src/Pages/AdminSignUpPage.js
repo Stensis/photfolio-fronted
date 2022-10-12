cce3bc970756c56a3d40af7a6e9a463216da09a8
Stensis <irenenjuguna98@gmail.com>
2022-10-12 11:01:42 +0300

    added adminsignup page

--- src/Pages/AdminSignUpPage.js
+++ src/Pages/AdminSignUpPage.js
@@ -1,31 +1,31 @@
-// import React from "react";
-// import CreateAccountForm from "../Components/createAccountComponent";
-// import Navbar from "../Components/navbarComponent";
+import React from "react";
+import CreateAccountForm from "../Components/createAccountComponent";
+import Navbar from "../Components/navbarComponent";
 
-// // sign-up page
-// function AdminSignUp() {
+// sign-up page
+function AdminSignUp() {
 
-//   return (
-//     <div>
-//       <Navbar />
-//       {/* card box */}
-//       <div class="d-flex justify-content-center mt-5">
-//         <div class="card border-primary mb-3">
-//           <div class="card-body">
-//             {/* login form for either users or admins */}
-//             <CreateAccountForm role="Admin" />
+  return (
+    <div>
+      <Navbar />
+      {/* card box */}
+      <div class="d-flex justify-content-center mt-5">
+        <div class="card border-primary mb-3">
+          <div class="card-body">
+            {/* login form for either users or admins */}
+            <CreateAccountForm role="Admin" />
 
-//             {/* sign-in and sign-up */}
-//             <div class="d-grid gap-2 mt-3 w-25">
-//               <button class="btn btn-lg btn-primary text-nowrap" type="button">
-//                 Sign Up
-//               </button>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+            {/* sign-in and sign-up */}
+            <div class="d-grid gap-2 mt-3 w-25">
+              <button class="btn btn-lg btn-primary text-nowrap" type="button">
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-// export default AdminSignUp;
+export default AdminSignUp;
