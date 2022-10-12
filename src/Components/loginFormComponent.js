5dd3b97533ef67955f07986c0aed043416130f0b
Stensis <irenenjuguna98@gmail.com>
2022-10-12 10:59:36 +0300

    added login form component

--- src/Components/loginFormComponent.js
+++ src/Components/loginFormComponent.js
@@ -1,52 +1,52 @@
-// import React from "react";
+import React from "react";
 
-// //login form for user
-// function LoginForm() {
-//   const userRole = "User Login";
-//   return (
-//     <div>
-//       <h4 class="card-title">{userRole}</h4>
-//       <p class="card-text">
-//         <form>
-//           <fieldset>
-//             <div class="form-group">
-//               <label for="userEmail" class="form-label mt-4">
-//                 Email address
-//               </label>
-//               <input
-//                 type="email"
-//                 class="form-control"
-//                 id="userEmail"
-//                 placeholder="Email"
-//               />
-//             </div>
-//             <div class="form-group">
-//               <label for="userPassword" class="form-label mt-4">
-//                 Password
-//               </label>
-//               <input
-//                 type="password"
-//                 class="form-control"
-//                 id="userPassword"
-//                 placeholder="Password"
-//               />
-//             </div>
-//             {/* sign-in and sign-up */}
-//             <div class="d-grid gap-2 mt-3 w-25">
-//               <button class="btn btn-lg btn-primary" type="button">
-//                 Login
-//               </button>
-//             </div>
-//             <div class="d-grid gap-2 mt-3">
-//               <button class="btn btn-lg btn-warning mt-2" type="button">
-//                 Don't Have an account? Create Account
-//               </button>
-//             </div>
-//           </fieldset>
-//         </form>
-//       </p>
-//     </div>
-//   );
-// }
+//login form for user
+function LoginForm() {
+  const userRole = "User Login";
+  return (
+    <div>
+      <h4 class="card-title">{userRole}</h4>
+      <p class="card-text">
+        <form>
+          <fieldset>
+            <div class="form-group">
+              <label for="userEmail" class="form-label mt-4">
+                Email address
+              </label>
+              <input
+                type="email"
+                class="form-control"
+                id="userEmail"
+                placeholder="Email"
+              />
+            </div>
+            <div class="form-group">
+              <label for="userPassword" class="form-label mt-4">
+                Password
+              </label>
+              <input
+                type="password"
+                class="form-control"
+                id="userPassword"
+                placeholder="Password"
+              />
+            </div>
+            {/* sign-in and sign-up */}
+            <div class="d-grid gap-2 mt-3 w-25">
+              <button class="btn btn-lg btn-primary" type="button">
+                Login
+              </button>
+            </div>
+            <div class="d-grid gap-2 mt-3">
+              <button class="btn btn-lg btn-warning mt-2" type="button">
+                Don't Have an account? Create Account
+              </button>
+            </div>
+          </fieldset>
+        </form>
+      </p>
+    </div>
+  );
+}
 
-// export default LoginForm;
+export default LoginForm;
