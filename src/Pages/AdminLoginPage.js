103fb0f0c58e38a010f53fb02321d4549c24d7da
Stensis <irenenjuguna98@gmail.com>
2022-10-12 11:01:06 +0300

    added admin login page

--- src/Pages/AdminLoginPage.js
+++ src/Pages/AdminLoginPage.js
@@ -1,25 +1,25 @@
-// import React from "react";
-// import Navbar from "../Components/navbarComponent";
-// import AdminLoginForm from "../Components/adminLoginFormComponent";
+import React from "react";
+import Navbar from "../Components/navbarComponent";
+import AdminLoginForm from "../Components/adminLoginFormComponent";
 
-// // login page
-// function AdminLogin() {
-//   return (
-//     <div>
-//       {/* navigation bar */}
-//       <Navbar />
+// login page
+function AdminLogin() {
+  return (
+    <div>
+      {/* navigation bar */}
+      <Navbar />
 
-//       {/* card box */}
-//       <div className="d-flex justify-content-center mt-5">
-//         <div className="card border-primary mb-3" style={{ maxWidth: "40rem" }}>
-//           <div className="card-body">
-//             {/* login form for either admin */}
-//             <AdminLoginForm />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+      {/* card box */}
+      <div className="d-flex justify-content-center mt-5">
+        <div className="card border-primary mb-3" style={{ maxWidth: "40rem" }}>
+          <div className="card-body">
+            {/* login form for either admin */}
+            <AdminLoginForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-// export default AdminLogin;
+export default AdminLogin;
