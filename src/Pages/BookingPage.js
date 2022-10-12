18affcd7f45c87c8d18ad3b39d4286df3c7cbc09
Stensis <irenenjuguna98@gmail.com>
2022-10-12 11:02:07 +0300

    added booking page

--- src/Pages/BookingPage.js
+++ src/Pages/BookingPage.js
@@ -1,17 +1,17 @@
-// import React from "react";
-// import Navbar from "../Components/navbarComponent";
-// import BookUs from "../Components/bookUsComponent";
+import React from "react";
+import Navbar from "../Components/navbarComponent";
+import BookUs from "../Components/bookUsComponent";
 
-// // booking page
-// function Booking() {
-//   return (
-//     <div>
-//       <Navbar />
-//       <div className="mt-4">
-//         <BookUs />
-//       </div>
-//     </div>
-//   );
-// }
+// booking page
+function Booking() {
+  return (
+    <div>
+      <Navbar />
+      <div className="mt-4">
+        <BookUs />
+      </div>
+    </div>
+  );
+}
 
-// export default Booking;
+export default Booking;
